a8425faa4c5455c9ba7c635e456a4590fbaee53b
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2022-06-14 12:04:19 +0700

    layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,144 +1,192 @@
-import React from 'react';
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 
 const Home = () => {
-    const defaultRow = 3;
-    const defaultCol = 4;
-    const [row, setRow] = useState(defaultRow);
-    const [col, setCol] = useState(defaultCol);
+  const defaultRow = 3;
+  const defaultCol = 4;
+  const [row, setRow] = useState(defaultRow);
+  const [col, setCol] = useState(defaultCol);
 
-    const handleRowChange = (rowValue) => {
-        setRow(rowValue);
-    };
+  const handleRowChange = (rowValue) => {
+    setRow(rowValue);
+  };
 
-    const handleColChange = (colValue) => {
-        setCol(colValue);
-    };
+  const handleColChange = (colValue) => {
+    setCol(colValue);
+  };
 
-    return (
-        <div className='container'>
-            <h1 className=''>Gauss Calculator</h1>
-            <div>
-                <MatriksSizeInput
-                    onColChange={handleColChange}
-                    onRowChange={handleRowChange}
-                    defaultRow={defaultRow}
-                    defaultCol={defaultCol}
-                />
-            </div>
-            <div>
-                <MatriksInput row={row} col={col} />
-            </div>
-        </div>
-    );
+  return (
+    <div className="container mx-auto p-4 max-w-md">
+      <h1 className="text-3xl font-bold mb-6">Gauss Calculator</h1>
+      <div className="mb-4">
+        <MatriksSizeInput
+          onColChange={handleColChange}
+          onRowChange={handleRowChange}
+          defaultRow={defaultRow}
+          defaultCol={defaultCol}
+        />
+      </div>
+      <div>
+        <MatriksInput row={row} col={col} />
+      </div>
+    </div>
+  );
 };
 export default Home;
 
 function MatriksSizeInput(props) {
-    const [row, setRow] = useState(props.defaultRow);
-    const [col, setCol] = useState(props.defaultCol);
-    const handleRowChange = (e) => {
-        setRow(e.target.value);
-    };
+  const [row, setRow] = useState(props.defaultRow);
+  const [col, setCol] = useState(props.defaultCol);
+  const handleRowChange = (e) => {
+    setRow(e.target.value);
+  };
 
-    const handleColChange = (e) => {
-        setCol(e.target.value);
-    };
+  const handleColChange = (e) => {
+    setCol(e.target.value);
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        props.onRowChange(row);
-        props.onColChange(col);
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.onRowChange(row);
+    props.onColChange(col);
+  };
 
-    const handleReset = (e) => {
-        setRow(props.defaultRow);
-        setCol(props.defaultCol);
-        props.onRowChange(props.defaultRow);
-        props.onColChange(props.defaultCol);
-    };
+  const handleReset = (e) => {
+    setRow(props.defaultRow);
+    setCol(props.defaultCol);
+    props.onRowChange(props.defaultRow);
+    props.onColChange(props.defaultCol);
+  };
 
-    return (
-        <form onSubmit={handleSubmit} onReset={handleReset}>
-            <SizeInput
-                onValueChange={handleRowChange}
-                defaultValue={props.defaultRow}
-                name="row"
-                label="Row"
-            />
-            <SizeInput
-                onValueChange={handleColChange}
-                defaultValue={props.defaultCol}
-                name="row"
-                label="Col"
-            />
-            <input type="submit" value={'Submit'} />
-            <input type="reset" value={'Reset'} />
-        </form>
-    );
+  return (
+    <form
+      onSubmit={handleSubmit}
+      onReset={handleReset}
+      className="flex flex-col gap-2"
+    >
+      <div className="flex gap-6">
+        <SizeInput
+          onValueChange={handleRowChange}
+          defaultValue={props.defaultRow}
+          name="row"
+          label="Row"
+        />
+        <SizeInput
+          onValueChange={handleColChange}
+          defaultValue={props.defaultCol}
+          name="col"
+          label="Col"
+        />
+      </div>
+    </form>
+  );
 }
 
 function SizeInput(props) {
-    return (
-        <div>
-            <label htmlFor={props.name}>{props.label}</label>
-            <input
-                className="input-matriksSize"
-                type="number"
-                id={props.name}
-                name={props.name}
-                min={2}
-                max={6}
-                defaultValue={props.defaultValue}
-                contentEditable
-                onChange={props.onValueChange}
-                required
-            />
+  const handleDecrease = (e) => {
+    console.log(props.row)
+  };
+
+  const handleIncrease = (e) => {
+    console.log(props.row)
+  };
+
+  return (
+    <div className="flex items-center gap-2">
+      <label htmlFor={props.name} className="font-semibold">
+        {props.label} :{" "}
+      </label>
+      <div className="flex">
+        <div className="flex flex-col">
+          <button
+            type="button"
+            onClick={handleDecrease}
+            className="border font-semibold aspect-square border-slate-300 inline-flex items-center justify-center w-6"
+          >
+            -
+          </button>
         </div>
-    );
+        <input
+          className="bg-white-300 border font-semibold border-slate-300 focus:outline-sky-600 text-center w-[3ch]"
+          id={props.name}
+          inputMode={"numeric"}
+          pattern={"[0-9]*"}
+          name={props.name}
+          min={2}
+          max={6}
+          defaultValue={props.defaultValue}
+          contentEditable
+          onChange={props.onValueChange}
+          disabled
+          required
+        />
+        <div className="flex flex-col">
+          <button
+            type="button"
+            onClick={handleIncrease}
+            className="border font-semibold aspect-square border-slate-300 inline-flex items-center justify-center w-6"
+          >
+            +
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 function RenderBoxInput(props) {
-    const MatriksBoxRender = [];
-    for (let i = 0; i < props.row; i++) {
-        const MatriksColumn = [];
-        for (let j = 0; j < props.col; j++) {
-            MatriksColumn.push(
-                <MatriksCol key={j} name={`matriks[${i}][${j}]`} />
-            );
-        }
-        MatriksBoxRender.push(<MatriksRow key={i}>{MatriksColumn}</MatriksRow>);
+  const MatriksBoxRender = [];
+  for (let i = 0; i < props.row; i++) {
+    const MatriksColumn = [];
+    for (let j = 0; j < props.col; j++) {
+      MatriksColumn.push(<MatriksCol key={j} name={`matriks[${i}][${j}]`} />);
     }
-    return <div>{MatriksBoxRender}</div>;
+    MatriksBoxRender.push(<MatriksRow key={i}>{MatriksColumn}</MatriksRow>);
+  }
+  return <div>{MatriksBoxRender}</div>;
 }
 
 function MatriksInput(props) {
-    return (
-        <form>
-            <div>
-                <RenderBoxInput row={props.row} col={props.col} />
-            </div>
-            <div>
-                <input type="submit" value="Submit" />
-            </div>
-        </form>
-    );
+  return (
+    <div>
+      <h2 className="text-xl font-bold mb-4">Input Your Matriks : </h2>
+      <form>
+        <div className="mb-2">
+          <RenderBoxInput row={props.row} col={props.col} />
+        </div>
+        <div className="flex flex-col gap-2 justify-end">
+          <button
+            type="submit"
+            className="bg-sky-600 text-white font-semibold px-4 py-1 rounded-md w-full"
+          >
+            Solve
+          </button>
+          <button
+            type="reset"
+            className="bg-red-600 text-white font-semibold px-4 py-1 rounded-md w-full"
+          >
+            Reset
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 function MatriksRow(props) {
-    return <div className="matriks-row">{props.children}</div>;
+  return <div className="flex w-full">{props.children}</div>;
 }
 
 function MatriksCol(props) {
-    return (
-        <input
-            type="number"
-            name={props.name}
-            id={props.name}
-            title={props.name}
-            required
-            defaultValue={0}
-            max="3"
-        />
-    );
+  return (
+    <input
+      className="flex-1 flex pl-2 border border-slate-300 w-full focus:outline-sky-600"
+      type="number"
+      name={props.name}
+      id={props.name}
+      title={props.name}
+      required
+      defaultValue={0}
+    />
+  );
 }
